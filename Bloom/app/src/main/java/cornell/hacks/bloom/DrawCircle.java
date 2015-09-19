da62135e040b8c4a6e4c0e7e4fd84ea7799bdb62
markz114 <mzhao114@gmail.com>
2015-09-19 09:26:01 -0400

    Polished Pulse Animation
    Added gradient to edge of pulse, added a non-linear pulse velocity.

--- Bloom/app/src/main/java/cornell/hacks/bloom/DrawCircle.java
+++ Bloom/app/src/main/java/cornell/hacks/bloom/DrawCircle.java
@@ -5,6 +5,7 @@ import android.animation.PropertyValuesHolder;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.BlurMaskFilter;
+import android.graphics.RadialGradient;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.os.Handler;
@@ -37,12 +38,12 @@ public class DrawCircle extends View {
     private final double SCALING_FACTOR = 3.75; //Scaling for max size
     private float blur;
     private final double DELAY_FACTOR = 500.0;
-    private final float VELOCITY_FACTOR = (float)20.0;  //Increase for slower velocity
+    private final float VELOCITY_FACTOR = (float)80.0;  //Increase for slower velocity
+    private final float DIFFERENCE_FACTOR = (float)0.05; //
     private final float DPI_FACTOR = (float)0.6;
     private Context context;
 
     public DrawCircle(Context context,int intensity){
-        //Empty Constructor
         super(context);
         this.intensity = intensity;
         paint.setAntiAlias(true);
@@ -53,6 +54,7 @@ public class DrawCircle extends View {
         alpha = 255;
         blur = (float) 0.0;
         this.context = context;
+        this.setLayerType(View.LAYER_TYPE_SOFTWARE, null);  //Hardware acceleration off
     }
 
     public void setIntensity(int intensity){
@@ -87,33 +89,55 @@ public class DrawCircle extends View {
         alpha = (int)((127/(MAX_RADIUS - MIN_RADIUS))*radius + (255 - (MAX_RADIUS*127/(MAX_RADIUS-MIN_RADIUS))));
 
         //Blur increases as the radius of the circle increases.
-        blur = radius*(MAX_BLUR-MIN_BLUR)/(MAX_RADIUS-MIN_RADIUS) +
-                MAX_BLUR - MAX_RADIUS*(MAX_BLUR-MIN_BLUR)/(MAX_RADIUS-MIN_RADIUS);
+        //blur = radius*(MAX_BLUR-MIN_BLUR)/(MAX_RADIUS-MIN_RADIUS) +
+        //        MAX_BLUR - MAX_RADIUS*(MAX_BLUR-MIN_BLUR)/(MAX_RADIUS-MIN_RADIUS);
 
         //Set radius of the circle
         float maxRadius = (float) (intensity * SCALING_FACTOR + MIN_RADIUS);
-        float velocity = (intensity/VELOCITY_FACTOR);
+
+
         if(radius <= MIN_RADIUS && growCircle == false){
             growCircle = true;
         }
         if(radius >= maxRadius && growCircle == true){
             growCircle = false;
         }
-
+        float centerDifference = (float) (Math.abs(this.radius - (maxRadius + MIN_RADIUS)/2.0));
+        float velocityAdd = (float)((maxRadius + MIN_RADIUS)/2.0 - MIN_RADIUS);
+        float radiusPosition = Math.abs(velocityAdd - centerDifference);
         if(growCircle == true){
+            //Velocity is proportional to the difference between max radius and radius
+            float velocity = (intensity/VELOCITY_FACTOR) + radiusPosition * DIFFERENCE_FACTOR;
+            System.out.println(velocity);
             this.radius = this.radius + velocity;
+
         }
         else{
+            float minDifference = this.radius - MIN_RADIUS;
+            float velocity = (intensity/VELOCITY_FACTOR) + radiusPosition * DIFFERENCE_FACTOR;
+            System.out.println("SHRINK");
+            System.out.println(velocity);
             this.radius = this.radius - velocity;
         }
 
 
         int blueInt = this.getResources().getColor(R.color.blue_500);
+        int lightblue = this.getResources().getColor(R.color.light_blue);
+        int superlightblue = this.getResources().getColor(R.color.super_light_blue);
+        int grayblue = this.getResources().getColor(R.color.gray_blue);
         paint.setColor(blueInt);
         paint.setStyle(Paint.Style.FILL);
-        paint.setMaskFilter(new BlurMaskFilter(blur, BlurMaskFilter.Blur.NORMAL));  //Blur
-        paint.setAlpha(alpha); //Transparency
+        //paint.setMaskFilter(new BlurMaskFilter(blur, BlurMaskFilter.Blur.OUTER));  //Blur
         Display disp = ((WindowManager)this.getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+
+        //create radial gradient
+        RadialGradient gradient = new android.graphics.RadialGradient(
+                (float)(disp.getWidth() / 2.0), (float) (disp.getHeight() / 1.75),
+                radius, new int[]{blueInt, 0xffffff}, new float[]{0.5f, 1.0f},
+                android.graphics.Shader.TileMode.CLAMP);
+
+        paint.setAlpha(alpha); //Transparency
+        paint.setShader(gradient);
         canvas.drawCircle((float)(disp.getWidth() / 2.0), (float) (disp.getHeight() / 1.75),
                 convertDpToPixel(this.radius*DPI_FACTOR,this.context), paint);
 
