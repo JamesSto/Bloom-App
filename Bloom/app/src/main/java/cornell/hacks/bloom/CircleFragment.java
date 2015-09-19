a16e5110f20d106d83e217fa75ee0f1beabe9d0f
Han Li <hli@p1analytics.com>
2015-09-19 01:21:21 -0400

    gps implementation

--- Bloom/app/src/main/java/cornell/hacks/bloom/CircleFragment.java
+++ Bloom/app/src/main/java/cornell/hacks/bloom/CircleFragment.java
@@ -1,6 +1,10 @@
 package cornell.hacks.bloom;
 
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +14,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 /**
  * Use the {@link CircleFragment#newInstance} factory method to
@@ -43,15 +48,87 @@ public class CircleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        LocationManager locationManager = (LocationManager)
+                getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(final Location loc) {
+//                Toast.makeText(
+//                        getBaseContext(),
+//                        "Location changed: Lat: " + loc.getLatitude() + " Lng: "
+//                                + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+//                String longitude = "Longitude: " + loc.getLongitude();
+//                String latitude = "Latitude: " + loc.getLatitude();
+                getLastBestLocation();
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                displayToast("GPS Enabled");
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                displayToast("GPS Disabled");
+            }
+        };
+
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 5000, 10, mLocationListener);
+
         View v = inflater.inflate(R.layout.fragment_circle, container, false);
         FrameLayout layoutContainer = (FrameLayout) v.findViewById(R.id.fragment_circle_container);
 
+        layoutContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLastBestLocation();
+            }
+        });
         DrawCircle x = new DrawCircle(getActivity());
         layoutContainer.addView(x);
 
         // Inflate the layout for this fragment
         return v;
     }
+    private void getLastBestLocation() {
+        Location here;
+        LocationManager locationManager = (LocationManager)
+                getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location locationNet = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        long GPSLocationTime = 0;
+        if (null != locationGPS) { GPSLocationTime = locationGPS.getTime(); }
+
+        long NetLocationTime = 0;
+
+        if (null != locationNet) {
+            NetLocationTime = locationNet.getTime();
+        }
+
+        if ( 0 < GPSLocationTime - NetLocationTime ) {
+            here = locationGPS;
+        }
+        else {
+            here =  locationNet;
+        }
+        String latitude = here.getLatitude()+"";
+        String longitude = here.getLongitude()+"";
+        displayToast(latitude + " " + longitude);
+    }
+    public void displayToast(String text)
+    {
+        Toast.makeText(getActivity(), text, Toast.LENGTH_LONG).show();
+    }
+
 
 
 }
--- Bloom/app/src/main/java/cornell/hacks/bloom/MainActivity.java
+++ Bloom/app/src/main/java/cornell/hacks/bloom/MainActivity.java
@@ -1,10 +1,17 @@
 package cornell.hacks.bloom;
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,10 +23,19 @@ import butterknife.ButterKnife;
 //Big Red Hacks 2015
 //Authors: Jimmy Stoyell, Yuqi Zhao, Han Li, Jesse Lupica
 import android.app.Activity;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.main_activity_viewpager)
     ViewPager viewPager;
+    GPSTracker gps;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,15 +47,17 @@ public class MainActivity extends AppCompatActivity {
         // Here you would declare which page to visit on creation
         viewPager.setAdapter(pagerAdapter);
         viewPager.setCurrentItem(1);
-    }
 
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }   
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -72,5 +90,44 @@ public class MainActivity extends AppCompatActivity {
         public int getCount() {
             return 2;
         }
+    }
+    private class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location loc) {
+            Toast.makeText(
+                    getBaseContext(),
+                    "Location changed: Lat: " + loc.getLatitude() + " Lng: "
+                            + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+            String longitude = "Longitude: " + loc.getLongitude();
+            String latitude = "Latitude: " + loc.getLatitude();
+
+//        /*------- To get city name from coordinates -------- */
+//            String cityName = null;
+//            Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+//            List<Address> addresses;
+//            try {
+//                addresses = gcd.getFromLocation(loc.getLatitude(),
+//                        loc.getLongitude(), 1);
+//                if (addresses.size() > 0)
+//                    System.out.println(addresses.get(0).getLocality());
+//                cityName = addresses.get(0).getLocality();
+//            }
+//            catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            String s = longitude + "\n" + latitude + "\n\nMy Current City is: "
+//                    + cityName;
+//            editLocation.setText(s);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
     }
 }
